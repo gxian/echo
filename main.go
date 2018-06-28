bbf156253ec952e69e011e0af54138776e36e18d
gaoxiang <gaoxiang.ian@gmail.com>
2018-06-28 20:36:59 +0800

    增加忽略配置；完成实现

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"net/http"
+)
+
+var (
+	addr string
+	path string
+)
+
+func init() {
+	flag.StringVar(&addr, "addr", ":8000", "addr=:8000")
+	flag.StringVar(&path, "path", "/", "path=/xxx")
+}
+
+func main() {
+	flag.Parse()
+	m := http.NewServeMux()
+	m.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		w.WriteHeader(http.StatusOK)
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return
+		}
+		w.Write(b)
+	})
+	http.ListenAndServe(addr, m)
+}
